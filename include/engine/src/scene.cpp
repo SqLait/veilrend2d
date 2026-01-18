6df9a63578b42b4764a3af4d802a35567ff8dea8
SqLait <119504721+SqLait@users.noreply.github.com>
2026-01-18 18:42:10 +0100

    fixed: scene missing vtable call

---
+++ include/engine/src/scene.cpp
@@ -0,0 +1,9 @@
+#include <core/scene/scene.hpp>
+
+void Scene::update(f32 delta_time) {
+
+}
+
+void Scene::draw() {
+
+}
--- include/engine/src/scenemanager.cpp
+++ include/engine/src/scenemanager.cpp
@@ -18,5 +18,4 @@ void SceneManager::update(f32 delta_time) {
 void SceneManager::switch_scene(i32 index) {
     i32 i = GMath::clamp(index, 0, scenes.size());
     current_scene = scenes[i];
-    Transform t;
 }
